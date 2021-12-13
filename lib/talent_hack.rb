af8e023d8410b65335c518d58dbdf27b810d82af
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2021-12-13 16:34:26 -0300

    v1.10.1

--- lib/talent_hack.rb
+++ lib/talent_hack.rb
@@ -1,8 +1,11 @@
 require 'talent_hack/controllers/api/authenticable'
 require 'talent_hack/errors/application_error'
 require 'talent_hack/models/application_model'
+require 'talent_hack/models/rakes/one_timer'
 require 'talent_hack/modules'
 require 'talent_hack/schemas'
 require 'talent_hack/serializers/application_serializer'
 require 'talent_hack/services'
 require 'talent_hack/structs/application_struct'
+
+require 'talent_hack/tasks/one_timers.rake'
